d90c0af57fdd6b03f79c74459d295a71cc613a70
unknown <208021204+aujohn77@users.noreply.github.com>
2025-12-31 00:49:48 +1000

    Fix Render build: init submodules in build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,11 @@
 # exit on error
 set -o errexit
 
+echo "🔁 Initializing submodules"
+git submodule sync --recursive
+git submodule update --init --recursive
+
+echo "📦 Installing dependencies"
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
